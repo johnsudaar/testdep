626402ee885fa59e3249d036d9b6b0843d09dab8
Jonathan Hurter <john@scalingo.com>
2017-05-31 20:42:05 +0200

    Re-adding pkgZ

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/johnsudaar/testpkg1"
+	"github.com/johnsudaar/testpkg2"
 )
 
 func main() {
 	fmt.Println("PKG1: " + testpkg1.Version())
+	fmt.Println("PKG2: " + testpkg2.Version())
 }
